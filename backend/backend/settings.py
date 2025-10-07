a9e50250fa2c57f6f5b51f68526e05a152cd34b6
4315 Лобанов К.Е. <lobanov1107@yandex.ru>
2025-10-08 00:50:02 +0300

    add ci/cd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ys-n857b3^)4a&gnar6#_=qkv&kgtm-u69%$hwg_mud^kewyhi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["77.91.76.14", "127.0.0.1", "localhost", "https://cleartime.sytes.net"]
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = "/app/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
