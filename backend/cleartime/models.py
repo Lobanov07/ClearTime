76f9d24768cf2a344f5aac024c9ef4ab41ec961e
Vladislav Voinov <voinoff.vlad2016@yandex.ru>
2025-10-08 14:59:20 +0300

    connected user and tasks + added api/user/{pk} handler to get tasks by user_id

--- backend/cleartime/models.py
+++ backend/cleartime/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
     title = models.CharField("Title", blank=False, max_length=128)
     description = models.CharField("Description", blank=True, max_length=256)
     is_completed = models.BooleanField('Is completed', default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    
+    def __str__(self) -> str:
+        return self.title
